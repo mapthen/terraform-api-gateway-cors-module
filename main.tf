e9c7260ffddc3f35547ec65938b36e8e0fc80949
Simon <rohrersm@gmail.com>
2020-05-07 13:45:38 +0200

    Update to terraform 0.2

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "aws_api_gateway_method" "ResourceOptions" {
-  rest_api_id = "${var.rest_api_id}"
-  resource_id = "${var.resource_id}"
+  rest_api_id = var.rest_api_id
+  resource_id = var.resource_id
   http_method = "OPTIONS"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "ResourceOptionsIntegration" {
-  rest_api_id = "${var.rest_api_id}"
-  resource_id = "${var.resource_id}"
-  http_method = "${aws_api_gateway_method.ResourceOptions.http_method}"
+  rest_api_id = var.rest_api_id
+  resource_id = var.resource_id
+  http_method = aws_api_gateway_method.ResourceOptions.http_method
   type = "MOCK"
   request_templates = { 
     "application/json" = <<PARAMS
@@ -19,9 +19,9 @@ PARAMS
 
 resource "aws_api_gateway_integration_response" "ResourceOptionsIntegrationResponse" {
   depends_on = ["aws_api_gateway_integration.ResourceOptionsIntegration"]
-  rest_api_id = "${var.rest_api_id}"
-  resource_id = "${var.resource_id}"
-  http_method = "${aws_api_gateway_method.ResourceOptions.http_method}"
+  rest_api_id = var.rest_api_id
+  resource_id = var.resource_id
+  http_method = aws_api_gateway_method.ResourceOptions.http_method
   status_code = "200"
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
@@ -32,8 +32,8 @@ resource "aws_api_gateway_integration_response" "ResourceOptionsIntegrationRespo
 
 resource "aws_api_gateway_method_response" "ResourceOptions200" {
   depends_on = ["aws_api_gateway_method.ResourceOptions"]
-  rest_api_id = "${var.rest_api_id}"
-  resource_id = "${var.resource_id}"
+  rest_api_id = var.rest_api_id
+  resource_id = var.resource_id
   http_method = "OPTIONS"
   status_code = "200"
   response_models = { "application/json" = "Empty" }
